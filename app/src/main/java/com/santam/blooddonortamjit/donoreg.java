6d91a2dce912ed3e180ad03d910e6b503a032594
Tamjit <sanowertamjit@gmail.com>
2017-05-16 20:37:39 +0600

    Connecting with GIT

---
+++ app/src/main/java/com/santam/blooddonortamjit/donoreg.java
@@ -0,0 +1,13 @@
+package com.santam.blooddonortamjit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class donoreg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donoreg);
+    }
+}
