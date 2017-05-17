421b01eabfa890b812b93cf74205703ff28d6626
Tamjit <sanowertamjit@gmail.com>
2017-05-17 15:06:34 +0600

    Connecting with GIT

---
+++ app/src/main/java/com/santam/blooddonortamjit/donorProfile.java
@@ -0,0 +1,13 @@
+package com.santam.blooddonortamjit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class donorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_profile);
+    }
+}
